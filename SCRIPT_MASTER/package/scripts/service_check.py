d2a76d2c97a182661e134af99ef08746caa50d3b
robinlin <robinlin@iii.org.tw>
2015-03-12 09:54:56 +0800

    Add Customized Service to Ambari

---
+++ SCRIPT_MASTER/package/scripts/service_check.py
@@ -0,0 +1,35 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Ambari Agent
+
+"""
+
+from resource_management import *
+from resource_management.libraries.functions.dynamic_variable_interpretation import copy_tarballs_to_hdfs
+import json
+
+class ScriptServiceCheck(Script):
+  def service_check(self, env):
+      config = Script.get_config()
+      data = json.dumps(config)
+      print 'The configuration text file: '+data
+      
+    
+if __name__ == "__main__":
+  ScriptServiceCheck().execute()
+
